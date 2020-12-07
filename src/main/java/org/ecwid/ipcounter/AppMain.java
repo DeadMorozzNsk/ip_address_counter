178024add6c75dd2c0db1d0afd5aa6d1bff7edf9
DeadMorozz <evlampejev@gmail.com>
2020-12-07 06:51:43 +0400

    my hypothesis

--- src/main/java/org/ecwid/ipcounter/AppMain.java
+++ src/main/java/org/ecwid/ipcounter/AppMain.java
@@ -2,31 +2,74 @@ package org.ecwid.ipcounter;
 
 import java.io.File;
 import java.io.IOException;
+import java.nio.charset.StandardCharsets;
+import java.nio.file.Files;
+import java.nio.file.Paths;
+import java.util.HashSet;
 
 import static org.ecwid.ipcounter.FileAnalyzer.*;
 
+
 public class AppMain {
+
+    static void updateProgress(double progressPercentage) {
+        final int width = 50; // progress bar width in chars
+
+        System.out.print("\r[");
+        int i = 0;
+        for (; i <= (int) (progressPercentage * width); i++) {
+            System.out.print("\u2588");
+        }
+        for (; i < width; i++) {
+            System.out.print(" ");
+        }
+        System.out.print("]");
+    }
+
     public static void main(String[] args) {
+        /*try {
+            for (double progressPercentage = 0.0; progressPercentage < 1.0; progressPercentage += 0.01) {
+                updateProgress(progressPercentage);
+                Thread.sleep(20);
+            }
+        } catch (InterruptedException e) {
+            e.printStackTrace();
+        }*/
         String path = "src\\main\\java\\org\\ecwid\\ipcounter\\files\\testSet.txt";
 
-        long t = System.currentTimeMillis(); /* start time measuring */
+        long t = 0;
         try {
             File file = new File(path);
             if (!file.exists()) {
-                createTestDataSet(10000000, path); /* create payload file if not exists */
+                createTestDataSet(10000000, path);  /* create payload file if not exists */
             }
+            t = System.currentTimeMillis();  /* start time measuring */
             countIpAddressesFromFile(path);
+            //naiveCountIpAddressesFromFile(path);
         } catch (IOException e) {
             e.printStackTrace();
         }
 
         /* stop the timer */
-        System.out.println("Time passed = " + (System.currentTimeMillis() - t) + " ms");
+        long msec = (System.currentTimeMillis() - t);
+        long sec = msec/1000;
+        long mins = sec/60;
+        System.out.println("Time passed = " + msec + " ms");
+        System.out.println("Time passed = " + mins );
         Runtime runtime = Runtime.getRuntime();
-//        runtime.gc();
         long memory = runtime.totalMemory() - runtime.freeMemory();
-        System.out.println("Used memory in bytes: " + memory);
         System.out.println("Used memory in kilobytes: " + bytesToKilobytes(memory));
         System.out.println("Used memory in megabytes: " + bytesToMegabytes(memory));
+    }
+
+    /**
+     * простейший путь подсчета количества уникальных строк
+     * @param path путь к файлу
+     * @throws IOException ошибки при чтении/получению доступа к файлу
+     */
+    private static void naiveCountIpAddressesFromFile(String path) throws IOException {
+        HashSet<String> al = new HashSet<>();
+        Files.lines(Paths.get(path), StandardCharsets.UTF_8).forEach(al::add);
+        System.out.println("unique addresses = " + al.size());
     }
 }
--- src/main/java/org/ecwid/ipcounter/FileAnalyzer.java
+++ src/main/java/org/ecwid/ipcounter/FileAnalyzer.java
@@ -1,8 +1,25 @@
 package org.ecwid.ipcounter;
 
 import java.io.*;
+import java.nio.ByteBuffer;
 import java.util.*;
 
+
+//    private static int convertByteArrayToIntValue(byte[] arr, int lastIndex) {
+//        int result = 0;
+//        int iter = 0;
+//        for (int i = lastIndex; i >= 0; i--) {
+//            result = (int) Math.pow(i, lastIndex);
+//        }
+//        if (lastIndex == 0) return arr[lastIndex];
+//        else if (lastIndex == 1) {
+//            result = arr[0] * 10 + arr[1];
+//        } else if (lastIndex == 2) {
+//            result = arr[0] * 100 + arr[1] * 10 + arr[2];
+//        }
+//        return result;
+//    }
+//}
 public final class FileAnalyzer {
     private static final long MEGABYTE = 1024L * 1024L;
     private static final long KILOBYTE = 1024L;
@@ -15,11 +32,22 @@ public final class FileAnalyzer {
         return bytes / KILOBYTE;
     }
 
+    /**
+     * генерирует случайное значние
+     * @param random генератор псевдослучайных чисел
+     * @return число в диапазоне 0..255 включительно
+     */
     private static int getRandomInt(Random random) {
         return random == null ?
-                new Random().nextInt(255) : random.nextInt(255);
+                new Random().nextInt(256) : random.nextInt(256);
     }
 
+    /**
+     * функция для создания тестового набора данных
+     * @param count количество записей в создаваемом наборе данных
+     * @param filePath путь к файлу набора данных
+     * @throws IOException ошибка потоков ввода/вывода
+     */
     public static void createTestDataSet(long count, String filePath) throws IOException {
         File file = new File(filePath);
         if (!file.exists()) {
@@ -47,19 +75,85 @@ public final class FileAnalyzer {
 
     }
 
+    /**
+     * подсчитывает приблизительное количтество уникальных ip-адресов в файле
+     * @param filePath путь к файлу
+     * @throws IOException ошибка ввода/вывода
+     */
     public static void countIpAddressesFromFile(String filePath) throws IOException {
         /* ИТАК, хитрый план:
         * читаем байтами по 50-100 мб
-        * символ \n = 10  для  сплита */
+        * байт символа \n = 10  для  сплита */
+
+        int rearingBlockSize = 50; /* размер считываемого блока в мегабайтах */
+
+        /* массив для записи статистики использования
+        * каждой из 4 честей адреса */
+        int[] partOne = new int[256];
+        int[] partTwo = new int[256];
+        int[] partThree = new int[256];
+        int[] partFour = new int[256];
+
+        int[][] stats = new int[4][256];
+        stats[0] = partOne;
+        stats[1] = partTwo;
+        stats[2] = partThree;
+        stats[3] = partFour;
 
         try (FileInputStream in = new FileInputStream(filePath)) {
-            byte[] arr = new byte[50*1024*1024];
+            byte[] arr = new byte[rearingBlockSize * 1024 * 1024];
+            byte[] tmp = new byte[16];
+            int tmpIndex = 0;
             int x;
-           List<String> ipAddresses = new ArrayList<>();
+            int stringsCount = 0;
+            int validStringsCount = 0;
             while ((x = in.read(arr)) != -1) {
-                ipAddresses.add(new String(arr, 0, x));
+                for (int i = 0; i < x; i++) {
+                    /*проходим по массиву байт  заданной длины
+                    * и заполняем временный массив до знака переноса строки
+                    * затем считываем строку, валидируем как ip-адрес
+                    * затем инкрементируем соответствующие массивы
+                    * в структуре сбора статистики использования */
+                    if (arr[i] != (byte) 10) {
+                        tmp[tmpIndex] = arr[i];
+                        tmpIndex++;
+                    } else {
+                        stringsCount++;
+                        String ipAddress = new String(tmp, 0, tmpIndex);
+                        tmpIndex = 0;
+                        if (addressIsValid(ipAddress)) {
+                            String[] parts = ipAddress.split("\\.");
+                            for (int j = 0; j < 4; j++) {
+                                stats[j][Integer.parseInt(parts[j])] += 1;
+                            }
+                            validStringsCount++;
+                        }
+                    }
+                }
             }
-            System.out.println("unique addresses = " + ipAddresses.size());
+            int uniques = 0;
+            for (int i = 0; i < 256; i++) {
+                int min = stats[0][i];
+                for (int j = 1; j < 4; j++) {
+                    min = Math.min(min, stats[j][i]);
+                }
+                uniques+= min;
+            }
+
+            System.out.println("strings count = " + stringsCount);
+            System.out.println("Valid strings count = " + validStringsCount);
+            System.out.println("Unique addresses approx. = " + uniques);
         }
     }
+
+    /**
+     * Проверка строки ip-адреса на соответствие шаблону
+     * @param address строковое представление ip адреса
+     * @return true - если соответствует шаблону, false - нет
+     */
+    private static boolean addressIsValid(String address) {
+        return address.matches("\\b((25[0-5]|2[0-4][0-9]|[01]?[0-9][0-9]?)(\\.|$)){4}\\b");
+    }
 }
+
+
